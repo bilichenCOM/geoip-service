6c9079fc8cfbe6af113a1b18d760807b4fa977c6
Mykhailo Bilichenko <bilichenko.mykhailo@gmail.com>
2019-12-12 11:43:01 +0200

    init-geoip-server

---
+++ geoip-server/src/test/java/com/bilichenko/geoipservice/GeoipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilichenko.geoipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
